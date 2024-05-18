e7a6938bb57ff993e0d3fbd211776b369f4b555d
pabmac <pablo93bio@gmail.com>
2024-05-18 17:32:56 +0200

    customizing

---
+++ src/constants/Tags.js
@@ -0,0 +1,65 @@
+import CSharp from "../icons/CSharp.astro";
+import Dart from "../icons/Dart.astro";
+import Flutter from "../icons/Flutter.astro";
+import Java from "../icons/Java.astro";
+import Javascript from "../icons/Javascript.astro";
+import Kotlin from "../icons/Kotlin.astro";
+import React from "../icons/React.astro";
+import Swift from "../icons/Swift.astro";
+import TailWind from "../icons/Tailwind.astro";
+import Typescript from "../icons/Typescript.astro";
+
+export const TAGS = {
+    FLUTTER: {
+        name: "Flutter",
+        class: "bg-black text-white",
+        icon: Flutter,
+    },
+    TAILWIND: {
+        name: "Tailwind",
+        class: "bg-black text-white",
+        icon: TailWind,
+    },
+    JAVASCRIPT: {
+        name: "JavaScript",
+        class: "bg-black text-white",
+        icon: Javascript,
+    },
+    TYPESCRIPT: {
+        name: "TypeScript",
+        class: "bg-black text-white",
+        icon: Typescript,
+    },
+    REACT: {
+        name: "React",
+        class: "bg-black text-white",
+        icon: React,
+    },
+    DART: {
+        name: "Dart",
+        class: "bg-black text-white",
+        icon: Dart,
+    },
+    CSHARP: {
+        name: "C-Sharp",
+        class: "bg-black text-white",
+        icon: CSharp,
+    },
+    JAVA: {
+        name: "Java",
+        class: "bg-black text-white",
+        icon: Java,
+    },
+    KOTLIN: {
+        name: "Kotlin",
+        class: "bg-black text-white",
+        icon: Kotlin,
+    },
+    SWIFT: {
+        name: "Swift",
+        class: "bg-black text-white",
+        icon: Swift,
+    },
+
+
+};
