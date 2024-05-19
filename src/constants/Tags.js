f0b6db7ae23453e164b008635e49c55092a4a659
pabmac <pablo93bio@gmail.com>
2024-05-19 08:34:07 +0200

    Skills, Icons Finished

--- src/constants/Tags.js
+++ src/constants/Tags.js
@@ -1,65 +1,71 @@
+import Angular from "../icons/Angular.astro";
+import AstroIcon from "../icons/AstroIcon.astro";
 import CSharp from "../icons/CSharp.astro";
+import Compose from "../icons/Compose.astro";
+import Critic from "../icons/Critic.astro";
+import Css from "../icons/Css.astro";
 import Dart from "../icons/Dart.astro";
+import Deno from "../icons/Deno.astro";
+import Docker from "../icons/Docker.astro";
+import Express from "../icons/Express.astro";
+import Firebase from "../icons/Firebase.astro";
+import Flexibility from "../icons/Flexibility.astro";
 import Flutter from "../icons/Flutter.astro";
+import Git from "../icons/Git.astro";
+import GitHub from "../icons/GitHub.astro";
+import Html from "../icons/Html.astro";
+import Iniciative from "../icons/Iniciative.astro";
 import Java from "../icons/Java.astro";
 import Javascript from "../icons/Javascript.astro";
 import Kotlin from "../icons/Kotlin.astro";
+import Learn from "../icons/Learn.astro";
+import Linux from "../icons/Linux.astro";
+import MongoDb from "../icons/MongoDb.astro";
+import Mysql from "../icons/Mysql.astro";
+import Node from "../icons/Node.astro";
+import Powhershell from "../icons/Powhershell.astro";
+import ProblemResolution from "../icons/ProblemResolution.astro";
 import React from "../icons/React.astro";
+import Spring from "../icons/Spring.astro";
 import Swift from "../icons/Swift.astro";
 import TailWind from "../icons/Tailwind.astro";
+import Team from "../icons/Team.astro";
 import Typescript from "../icons/Typescript.astro";
 
 export const TAGS = {
-    FLUTTER: {
-        name: "Flutter",
-        class: "bg-black text-white",
-        icon: Flutter,
-    },
-    TAILWIND: {
-        name: "Tailwind",
-        class: "bg-black text-white",
-        icon: TailWind,
-    },
-    JAVASCRIPT: {
-        name: "JavaScript",
-        class: "bg-black text-white",
-        icon: Javascript,
-    },
-    TYPESCRIPT: {
-        name: "TypeScript",
-        class: "bg-black text-white",
-        icon: Typescript,
-    },
-    REACT: {
-        name: "React",
-        class: "bg-black text-white",
-        icon: React,
-    },
-    DART: {
-        name: "Dart",
-        class: "bg-black text-white",
-        icon: Dart,
-    },
-    CSHARP: {
-        name: "C-Sharp",
-        class: "bg-black text-white",
-        icon: CSharp,
-    },
-    JAVA: {
-        name: "Java",
-        class: "bg-black text-white",
-        icon: Java,
-    },
-    KOTLIN: {
-        name: "Kotlin",
-        class: "bg-black text-white",
-        icon: Kotlin,
-    },
-    SWIFT: {
-        name: "Swift",
-        class: "bg-black text-white",
-        icon: Swift,
-    },
-
-
+    FLUTTER: { name: "Flutter", icon: Flutter},
+    TAILWIND: {name: "Tailwind",icon: TailWind},
+    JAVASCRIPT: {name: "JavaScript",icon: Javascript},
+    TYPESCRIPT: {name: "TypeScript",icon: Typescript},
+    REACT: {name: "React",icon: React},
+    DART: {name: "Dart",icon: Dart},
+    CSHARP: {name: "C-Sharp",icon: CSharp},
+    JAVA: {name: "Java",icon: Java},
+    KOTLIN: {name: "Kotlin",icon: Kotlin},
+    SWIFT: {name: "Swift",icon: Swift},
+    SWIFTUI: {name: "Swift UI",icon: Swift},
+    ANGULAR: {name: "Angular", icon: Angular},
+    REACT: {name: "React", icon: React},
+    NODE: {name: "Node", icon: Node},
+    DENO: {name: "Deno", icon: Deno},
+    COMPOSE: {name: "Compose Multiplatform", icon: Compose},
+    SPRING: {name: "Spring", icon: Spring},
+    EXPRESS: {name: "Express", icon: Express},
+    ASTRO: {name: "Astro", icon: AstroIcon},
+    FLEXIBILITY: {name: "Flexibilidad y Adaptabilidad", icon: Flexibility},
+    TEAMWORK: {name: "Trabajo en Equipo", icon: Team},
+    INITIATIVE: {name: "Proactividad", icon: Iniciative},
+    CRITIC: {name: "Pensamiento Crítico", icon: Critic},
+    PROBLEMRES: {name: "Resolución de Problemas", icon: ProblemResolution},
+    LEARN: {name: "Siempre Aprendiendo", icon: Learn},
+    GIT: {name: "Git", icon: Git},
+    GITHUB: {name: "Github", icon: GitHub},
+    LINUX: {name: "Bash - Zsh", icon: Linux},
+    POWERSHELL: {name: "PowerShell", icon: Powhershell},
+    DOCKER: {name: "Docker", icon: Docker},
+    MYSQL: {name: "MySql", icon: Mysql},
+    MONGODB: {name: "MongoDb", icon: MongoDb},
+    FIREBASE: {name: "Firebase", icon: Firebase},
+    HTML: {name: "HTML", icon: Html},
+    CSS: {name: "CSS", icon: Css},
 };
